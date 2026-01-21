78c75631794d2e10fc2f6d18db0599f2a5435d30
Nitishroy1 <nitishk9199@gmail.com>
2026-01-21 18:23:09 +0530

    patient data add, remove, view, retrive

--- src/com/packone/JdbcPro5.java
+++ src/com/packone/JdbcPro5.java
@@ -3,6 +3,7 @@ package com.packone;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLIntegrityConstraintViolationException;
 
 public class JdbcPro5 {
@@ -27,21 +28,26 @@ public class JdbcPro5 {
 	}
 	//patient data
 	public void patientOperation() {
-		try 
-		{
-			Connection con= connect();
+		Connection con= connect();
+		try(con) 
+		     {
+			
 			String sql = "INSERT INTO  PATIENT VALUES (?, ?, ?, ?)";
 			PreparedStatement pstmt1 = con.prepareStatement(sql);
+			PreparedStatement pstmt2=con.prepareStatement("select * from patient");
+			PreparedStatement pstmt3=con.prepareStatement("select * from patient where pid=?");
+			PreparedStatement pstmt4=con.prepareStatement("update patient set age=? where pid=?");
+			PreparedStatement pstmt5=con.prepareStatement("delete from patient where pid=?");
 			
 			while(true) {
 				
 				IO.println("-----------patient data----------");
 				IO.println("1. Addding patient Data: ");
-				IO.println("2. Addding patient Data: ");
-				IO.println("1. Addding patient Data: ");
-				IO.println("1. Addding patient Data: ");
-				IO.println("1. Addding patient Data: ");
-				IO.println("1. Addding patient Data: ");
+				IO.println("2. View patient Data: ");
+				IO.println("3. retrive patient Data: ");
+				IO.println("4. Update patient Data: ");
+				IO.println("5. delete patient Data: ");
+				IO.println("6.  Exit ");
 				
 				int choice=Integer.parseInt(IO.readln("Enter your choice: "));
 				switch(choice) {
@@ -66,6 +72,58 @@ public class JdbcPro5 {
 				 }
 					
 				}
+				case 2->{
+					IO.println("View patient data:");
+					ResultSet rs=pstmt2.executeQuery();
+					while(rs.next()) {
+						IO.println(rs.getObject(1)+" "+ rs.getObject(2)+" "+ rs.getObject(3)+" "+rs.getObject(4));
+					}
+					IO.println("View the patient data don");
+					
+				}
+				case 3->{
+					IO.println("retrive patient data : ");
+					String id=IO.readln("Enter the patient id which you want to retrive:");
+					pstmt3.setString(1, id);
+					ResultSet rs=pstmt3.executeQuery();
+					while(rs.next()) {
+						IO.println(rs.getObject(1)+" "+ rs.getObject(2)+" "+ rs.getObject(3)+" "+rs.getObject(4));
+					}
+					
+				}
+				case 4->{
+					int age=Integer.parseInt(IO.readln("Enter patent New Age: "));
+					pstmt4.setInt(1, age);
+					
+					String id=IO.readln("Enter the patient id which you want to Update:");
+					pstmt4.setString(2, id);
+					
+					int rowCount= pstmt4.executeUpdate();
+					if(rowCount<0) {
+						IO.println("no row will we updated. patient with  pid: "+id+" will not exite");
+					}
+					else {
+						IO.println(rowCount+"Patient with  pid: "+id+" record will be updated");
+					}
+					
+				}
+				case 5->{
+					String id=IO.readln("Enter the patient id which you want to delete:");
+					pstmt5.setString(1, id);
+					int rowCount= pstmt5.executeUpdate();
+					if(rowCount<0) {
+						IO.println("no row will we deleted. patient with  pid: "+id+" will not exite");
+					}
+					else {
+						IO.println(rowCount+"Patient with  pid: "+id+" record will be deleted");
+					}
+					
+				}
+				case 6->{
+					IO.println("Thanks for vesiting: ");
+					System.exit(0);
+					
+				}
 				default ->{
 					IO.println(" your choice is wrong , plz select correct choice");
 				}
@@ -81,6 +139,7 @@ public class JdbcPro5 {
 		{
 	       e.printStackTrace();
 		}
+		
 		
 		
 	}
